de0190445e39c1bd190b08a1bc422c306ee3157d
Micah Cowsik-Herstand <micah@micah.local>
2020-10-13 02:04:54 -0500

    small fix to trigger build

--- reward-points/src/pages/404.js
+++ reward-points/src/pages/404.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./index.module.css";
 
 export default function NoPage() {
-  return <main>Nothing to see here</main>;
+  return <main>Nothing to see here!</main>;
 }
